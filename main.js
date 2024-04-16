e9d18814a3ab65fee551ff1d9cea6d7f9c1b2b89
Aaron Smith <smithaaron654@gmail.com>
2024-04-16 16:32:29 -0400

    Changes made to whole project

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
+const gridButton = document.getElementById("gridButton");
 const grid = document.querySelector('.grid');
+
+
+gridButton.addEventListener('click', function () {
+  const gridSizeInput = prompt("What size grid would you like?");
+
+  if (gridSizeInput > 100 || gridSizeInput <= 0) {
+    alert("Invalid number. Please enter a number between 1 and 100.");
+  } else {
+    grid.innerHTML = "";
+    createGrid(gridSizeInput);
+  }
+});
+
+function createGrid(gridSizeInput) {
+
+  let newGridSize = gridSizeInput;
+
+  for (let i = 0; i < (newGridSize * newGridSize); i++) {
+    let box = document.createElement("div");
+    grid.appendChild(box).className = "box";
+    
+  }
+
+}
 
 grid.addEventListener('mouseover', function (e) {
   // Add the "active" class to only divs with a "box" class
@@ -6,3 +31,5 @@ grid.addEventListener('mouseover', function (e) {
     e.target.classList.add('active');
   }
 });
+
+createGrid(16);
